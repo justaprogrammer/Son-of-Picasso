b107db6839a79885698797a96532d205cb5bb61b
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-09-03 13:01:59 -0400

    More stuff

--- PicasaReboot.Core/ImageFileSystemService.cs
+++ PicasaReboot.Core/ImageFileSystemService.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Abstractions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PicasaReboot.Core
 {
@@ -14,6 +10,13 @@ namespace PicasaReboot.Core
         public ImageFileSystemService(IFileSystem fileSystem)
         {
             FileSystem = fileSystem;
+        }
+
+        public string LibraryDirectory { get; set; }
+
+        public IObservable<ImageFile> ListFiles()
+        {
+            throw new NotImplementedException();
         }
     }
 }
