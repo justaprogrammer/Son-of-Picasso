c9a70fc395f80e165e752bd62fa4b8fe04f3ee7a
Stanley Goldman <Stanley.Goldman@gmail.com>
2017-09-03 12:33:07 -0400

    Add project files.

---
+++ PicasaReboot.Core/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicasaReboot.Core
+{
+    public class ImageFileSystemService
+    {
+        protected IFileSystem FileSystem { get; }
+
+        public ImageFileSystemService(IFileSystem fileSystem)
+        {
+            FileSystem = fileSystem;
+        }
+    }
+}
---
+++ PicasaReboot.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PicasaReboot.Core;
+
+namespace PicasaReboot.Tests
+{
+    [TestClass]
+    public class ImageFileSystemServiceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var imageFileSystemService = new ImageFileSystemService(new MockFileSystem());
+        }
+    }
+}
