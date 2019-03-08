0c8f035a4c570249c0482ac8e6e82ae0f1aa3109
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-03-07 21:31:27 -0500

    Fixing build

--- src/SonOfPicasso.UI.Tests/ViewModels/ApplicationViewModelTests.cs
+++ src/SonOfPicasso.UI.Tests/ViewModels/ApplicationViewModelTests.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading;
-using DynamicData;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
