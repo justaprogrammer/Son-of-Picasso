90fc30b1752518a6324766e5034cee1a60a70bbb
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-09-30 08:35:20 -0400

    Adding a check

--- src/SonOfPicasso.UI.Tests/ViewModels/ApplicationViewModelTests.cs
+++ src/SonOfPicasso.UI.Tests/ViewModels/ApplicationViewModelTests.cs
@@ -1,4 +1,7 @@
 ﻿using Autofac.Extras.NSubstitute;
+using FluentAssertions;
+using NSubstitute;
+using SonOfPicasso.Core.Interfaces;
 using SonOfPicasso.Core.Scheduling;
 using SonOfPicasso.Testing.Common;
 using SonOfPicasso.Testing.Common.Scheduling;
@@ -23,8 +26,16 @@ namespace SonOfPicasso.UI.Tests.ViewModels
                 var testSchedulerProvider = new TestSchedulerProvider();
                 autoSub.Provide<ISchedulerProvider>(testSchedulerProvider);
 
+                var imageManagementService = autoSub.Resolve<IImageManagementService>();
+
                 var applicationViewModel = autoSub.Resolve<ApplicationViewModel>();
                 applicationViewModel.Activator.Activate();
+
+                imageManagementService.Received(1)
+                    .GetImagesWithDirectoryAndExif();
+
+                imageManagementService.Received(1)
+                    .GetAllAlbumsWithAlbumImages();
             }
         }
     }
