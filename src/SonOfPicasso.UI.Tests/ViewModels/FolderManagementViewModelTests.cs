c047527df91f857c70b5a56470d17e21927ba27c
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-10-18 00:38:40 -0400

    Start of a folder management view model

---
+++ src/SonOfPicasso.UI.Tests/ViewModels/FolderManagementViewModelTests.cs
@@ -0,0 +1,23 @@
+﻿using SonOfPicasso.UI.ViewModels;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SonOfPicasso.UI.Tests.ViewModels
+{
+    public class FolderManagementViewModelTests : ViewModelTestsBase
+    {
+        public FolderManagementViewModelTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public void ShouldInitializeAndActivate()
+        {
+            MockFileSystem.AddDirectory("D:\\");
+
+            var folderManagementViewModel = AutoSubstitute.Resolve<FolderManagementViewModel>();
+            folderManagementViewModel.Activator.Activate();
+        }
+    }
+}
---
+++ src/SonOfPicasso.UI/ViewModels/FolderManagementViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using DynamicData.Binding;
+using ReactiveUI;
+using SonOfPicasso.UI.ViewModels.Abstract;
+
+namespace SonOfPicasso.UI.ViewModels
+{
+    public class FolderManagementViewModel: ViewModelBase
+    {
+        public class FolderViewModel
+        {
+            public FolderViewModel(IDirectoryInfo directoryInfo)
+            {
+                Path = directoryInfo.FullName;
+                Children = new ObservableCollectionExtended<FolderViewModel>(directoryInfo.EnumerateDirectories()
+                    .Select(info => new FolderViewModel(info)));
+
+
+            }
+
+            public IObservableCollection<FolderViewModel> Children { get; }
+
+            public string Path { get; }
+        }
+
+        private readonly IFileSystem _fileSystem;
+
+        public FolderManagementViewModel(ViewModelActivator activator, IFileSystem fileSystem) : base(activator)
+        {
+            _fileSystem = fileSystem;
+
+            this.WhenActivated((CompositeDisposable disposable) =>
+            {
+                var driveInfos = _fileSystem.DriveInfo.GetDrives()
+                    .Select(driveInfo => driveInfo.RootDirectory)
+                    .Select(directoryInfo => new FolderViewModel(directoryInfo))
+                    .ToArray();
+            });
+        }
+    }
+}
