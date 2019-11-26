f47fa0986eaa1319d37668da824181d50afa5397
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-11-26 10:50:01 -0500

    Tweaking build

---
+++ build.ps1
@@ -0,0 +1,40 @@
+function printHeader {
+    param (
+        $text
+    )
+
+    Write-Host "**** $text ****"
+}
+
+printHeader "Clean"
+
+dotnet clean .\SonOfPicasso.sln -v m
+Remove-Item -Recurse .\reports
+
+printHeader "Publish"
+
+dotnet publish -c Release .\SonOfPicasso.sln -v m
+
+printHeader "Test"
+
+function test {
+    param (
+        $project
+    )
+
+    dotnet vstest src\$project\bin\Release\netcoreapp3.0\publish\$project.dll `
+        --logger:"trx;LogFileName=$project.trx" `
+        --ResultsDirectory:reports `
+        --collect:"XPlat code coverage" `
+        --settings:"src\coverletArgs.runsettings" | Tee-Object -Variable cmdOutput 
+  
+    $match = Select-String -Pattern "Attachments:\s+(.*?opencover.xml)" -InputObject $cmdOutput
+    $coverageFile = Resolve-Path -Relative $match.Matches[0].Groups[1].Value
+    Move-Item  $coverageFile ".\reports\$project.opencover.xml"
+}
+
+test "SonOfPicasso.Core.Tests"
+test "SonOfPicasso.Data.Tests"
+test "SonOfPicasso.Integration.Tests"
+test "SonOfPicasso.Tools.Tests"
+test "SonOfPicasso.UI.Tests"
