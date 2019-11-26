8c188af67c8f63102da90cfcecb6f0d41b14345c
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-11-25 19:37:32 -0500

    Moving some more stuff around

--- src/SonOfPicasso.Integration.Tests/IntegrationTestsBase.cs
+++ src/SonOfPicasso.Integration.Tests/IntegrationTestsBase.cs
@@ -14,7 +14,7 @@ using SonOfPicasso.Data.Repository;
 using SonOfPicasso.Data.Services;
 using SonOfPicasso.Testing.Common;
 using SonOfPicasso.Tools.Services;
-using SonOfPicasso.UI.Services;
+using SonOfPicasso.UI.WPF.Services;
 using Xunit.Abstractions;
 
 namespace SonOfPicasso.Integration.Tests
